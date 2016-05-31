f680c02ecbbe7010739d02a3bf8f54aa2da1eec3
dobo99 <engjs77@naver.com>
2016-05-31 16:05:52 +0900

    do prob.2

--- problem2.py
+++ problem2.py
@@ -2,22 +2,18 @@
 
 # 짝수이면서 4백만 이하인 피보나치 수열 항들의 합은?
 
-def fib(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
+x = [1, 2]
+
+while x[-1] <= 4000000:
+    number = x[-2] + x[-1]
+    x.append(number)
+
+y = []
+for num in x:
+    if num % 2 == 0:
+        y.append(num)
     else:
-        return fib(n-2) + fib(n-1)
+        del num
 
-x = []
-n = 2
-
-while fib(n) > 4000000:
-    if fib(n) % 2 == 0:
-        x.append(fib(n))
-        fib(n) == fib(n+1)
-    else:
-        fib(n) == fib(n+1)
-
-print x
+print y
+print sum(y)
