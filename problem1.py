768f3b077c049ba559c576bbbaa6a216884dc744
dobo99 <engjs77@naver.com>
2016-05-31 16:17:54 +0900

    add comments

--- problem1.py
+++ problem1.py
@@ -2,11 +2,12 @@
 
 # 1000보다 작은 자연수 중 3 또는 5의 배수를 모두 더하면?
 
-x = range(1, 1000)
-y = []
+x = range(1, 1000) # 1000보다 작은 자연수 리스트
+y = [] # 계산을 위한 빈 리스트
 
 for number in x:
     if number % 3 == 0 or number % 5 == 0:
         y.append(number)
+# x 리스트에 있는 수들 중 3의 배수나 5의 배수를 y리스트로 넣는다.
 
-print sum(y)
+print sum(y) # y속 수들의 총합
--- problem2.py
+++ problem2.py
@@ -2,11 +2,12 @@
 
 # 짝수이면서 4백만 이하인 피보나치 수열 항들의 합은?
 
-x = [1, 2]
+x = [1, 2] # 피보나치 수열의 시작점
 
 while x[-1] <= 4000000:
     number = x[-2] + x[-1]
     x.append(number)
+# 4백만 이하의 피보나치 수열을 구한다. append는 리스트의 오른쪽에 들어간다.
 
 y = []
 for num in x:
@@ -14,6 +15,8 @@ for num in x:
         y.append(num)
     else:
         del num
+# x는 4백만 이하의 피보나치 수열이다. 그 중 짝수를 y에 넣는다
 
 print y
 print sum(y)
+# 짝수인 수열과 그 총합
